9729334e8242acee01e8b43784eaca5eeb23bb4d
Niklas Schmidts <niklas@schmidts.dev>
2023-12-19 21:33:55 +0100

    Update Skills component styling for better mobile view

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,33 +15,41 @@ function Skills() {
       className="bg-[#111111] grid grid-cols-2 gap-8 p-3"
       style={{ gridTemplateColumns: "2fr 1fr" }}
     >
-      <div className="text-white font-bold text-xl w-full h-screen  grid grid-cols-2 gap-4 text-center ml-10">
+      <div className="text-white font-bold md:text-xl text-base w-full h-screen  grid grid-cols-2 gap-4 text-center ml-10">
         <div className="hover:scale-110 duration-300">
-          <img src={CSS} alt="CSS" className="w-20 mx-auto" />
+          <img src={CSS} alt="CSS" className="md:w-20 w-14 mx-auto" />
           <p>CSS</p>
         </div>
-        <div className="text-white font-bold text-xl hover:scale-110 duration-300">
-          <img src={HTML} alt="HTML" className="w-20 mx-auto" />
+        <div className="text-white font-bold md:text-xl text-base hover:scale-110 duration-300">
+          <img src={HTML} alt="HTML" className="md:w-20 w-14 mx-auto" />
           <p>HTML</p>
         </div>
-        <div className="text-white font-bold text-xl hover:scale-110 duration-300">
-          <img src={JavaScript} alt="JavaScript" className="w-20 mx-auto" />
+        <div className="text-white font-bold tmd:text-xl ext-base hover:scale-110 duration-300">
+          <img
+            src={JavaScript}
+            alt="JavaScript"
+            className="md:w-20 w-14 mx-auto"
+          />
           <p>JavaScript</p>
         </div>
-        <div className="text-white font-bold text-xl hover:scale-110 duration-300">
-          <img src={ReactLogo} alt="React" className="w-20 mx-auto" />
+        <div className="text-white font-bold md:text-xl text-base hover:scale-110 duration-300">
+          <img src={ReactLogo} alt="React" className="md:w-20 w-14 mx-auto" />
           <p>React</p>
         </div>
-        <div className="text-white font-bold text-xl hover:scale-110 duration-300">
-          <img src={TailwindLogo} alt="Tailwind" className="w-20 mx-auto" />
+        <div className="text-white font-bold md:text-xl text-base hover:scale-110 duration-300">
+          <img
+            src={TailwindLogo}
+            alt="Tailwind"
+            className="md:w-20 w-14 mx-auto"
+          />
           <p>Tailwind</p>
         </div>
       </div>
-      <div className="ml-5 mr-10 pl-10">
+      <div className="ml-5 mr-10 pl-5">
         <p className="text-white text-2xl font-bold border-b-4 inline">
           Skills
         </p>
-        <button className="text-white flex items-center border-solid border-2 border-white p-1 pl-2 mt-2 hover:p-3 duration-300">
+        <button className="text-white text-sm md:text-base flex items-center border-solid border-2 border-white p-1 pl-2 mt-2 hover:p-3 duration-300">
           <Link to="contact" smooth={true} duration={500}>
             View Contact
           </Link>
