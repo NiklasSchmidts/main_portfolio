f8a67c2d97b7e2a1bf1308843bc157b4f28cbfd3
Niklas Schmidts <niklas@schmidts.dev>
2023-12-20 12:32:46 +0100

    Update Bottom and Cursor components

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -5,10 +5,12 @@ import { Link } from "react-scroll";
 function Bottom() {
   return (
     <div className="bg-white w-full h-[40px]">
-      <div className="h-full flex justify-between items-center ml-20 mr-20">
+      <div className="h-full flex items-center ml-20 mr-20">
         <h1 className="font-bold text-xs md:text-lg">Copyright © 2023 Niklas Schmidts</h1>
+      </div>
+      <div className="fixed z-10 bottom-10 right-10">
         <Link to="home" smooth={true} duration={500}>
-          <ArrowUp className="text-black text-4xl cursor-pointer"></ArrowUp>
+          <ArrowUp className="text-white text-5xl cursor-pointer"></ArrowUp>
         </Link>
       </div>
     </div>
